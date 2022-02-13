323ed55107aec3b67b63c86ddffedfd0406d3012
nitin443 <imnitin55@gmail.com>
2022-02-13 11:56:23 +0530

    add middleware

---
+++ middleware/isAuth.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+ const authHeader = req.get('Authorization');
+
+ if(!authHeader){
+     const er = new Error('not authenticated');
+     res.status(401).json({message: er.message});
+ };
+
+ const token = authHeader.split(' ')[1];
+
+ let decodeToken;
+ try {
+     decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+ } catch (error) {
+    const er = new Error('Error in decode token');
+    res.status(500).json({message: er.message});
+ }
+
+ if(!decodeToken){
+    const er = new Error('not authenticated');
+    res.status(401).json({message: er.message});
+ }
+
+ req.userId = decodeToken.userId;
+ next();
+};
