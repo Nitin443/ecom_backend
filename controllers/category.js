28bae9592c5956dc8052b84dc2245665c73bc6c7
nitin443 <imnitin55@gmail.com>
2022-02-16 10:23:09 +0530

    add category api

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,16 @@
 const Category = require('../models/category');
 const Joi = require('joi');
+const {isNull} = require('lodash');
 
 exports.createCategory = (req, res) => {
-    
-    (async() => {
+
+    (async () => {
         try {
 
             /**
              * joi validation
              */
-             const schema = Joi.object({
+            const schema = Joi.object({
                 name: Joi.string().required(),
             });
 
@@ -22,11 +23,11 @@ exports.createCategory = (req, res) => {
             const { error, value } = schema.validate(req.body, options);
 
             if (error) {
-            return res.status(400).json({ errorMessage: error.details });
+                return res.status(400).json({ errorMessage: error.details });
             }
 
-            if(req.role === 0){
-                return res.status(400).json({message: 'you can not create category. only admin can create'})
+            if (req.role === 0) {
+                return res.status(400).json({ message: 'you can not create category. only admin can create' })
             }
 
             const category = new Category({
@@ -34,11 +35,174 @@ exports.createCategory = (req, res) => {
             });
 
             await category.save();
-            return res.status(200).json({ Category: category});
-            
+            return res.status(200).json({ Category: category });
+
         } catch (error) {
             const er = new Error('There is some error')
             return res.status(400).json({ message: er.message });
         }
     })();
 };
+
+exports.getCategoryById = (req, res) => {
+
+    (async () => {
+        try {
+            /**
+            * joi validation
+            */
+            const schema = Joi.object({
+                categoryId: Joi.string().required(),
+            });
+
+            // schema options
+            const options = {
+                abortEarly: false, // include all errors
+                allowUnknown: true, // ignore unknown props
+            };
+
+            const { error, value } = schema.validate(req.params, options);
+
+            if (error) {
+                return res.status(400).json({ errorMessage: error.details });
+            }
+
+            if (req.role === 0) {
+                return res.status(400).json({ message: 'you can not get category. only admin can get ' })
+            }
+
+            const categoryId = req.params.categoryId;
+
+            const getCategory = await Category.findById(categoryId);
+
+            if (isNull(getCategory)) {
+                return res.status(422).json({ message: 'Product with given id is not exit.' });
+            }
+
+            return res.status(200).json({ Category: getCategory });
+
+        } catch (error) {
+            const er = new Error('There is some error')
+            return res.status(400).json({ message: er.message });
+        }
+    })();
+
+}
+
+exports.getCategory = (req, res) => {
+    (async () => {
+        try {
+
+            if (req.role === 0) {
+                return res.status(400).json({ message: 'you can not get category. only admin can get ' })
+            }
+
+            const category = await Category.find();
+            return res.status(200).json({ Category: category });
+
+
+        } catch (error) {
+            const er = new Error('There is some error')
+            return res.status(400).json({ message: er.message });
+        }
+    })();
+}
+
+
+exports.updateCategory = (req, res) =>{
+    (async() => {
+        try {
+          /**
+            * joi validation
+            */
+           const schema = Joi.object({
+            categoryId: Joi.string().required(),
+        });
+
+        // schema options
+        const options = {
+            abortEarly: false, // include all errors
+            allowUnknown: true, // ignore unknown props
+        };
+
+        const { error, value } = schema.validate(req.params, options);
+
+        if (error) {
+            return res.status(400).json({ errorMessage: error.details });
+        }
+
+        if (req.role === 0) {
+            return res.status(400).json({ message: 'you can not update Category. only admin can update'})
+        }
+
+         
+        const categoryId = req.params.categoryId;
+
+        const updateContent = req.body;
+                let opts = {
+                    runValidators: true,
+                    setDefaultsOnInsert: true,
+                    upsert: false,
+                    context: 'query'
+                };
+
+        const updateCategory = await Category.findByIdAndUpdate(categoryId, updateContent, opts);
+         
+        if(isNull(updateCategory)){
+            return res.status(422).json({ message: 'Product with given id is not exit.'});
+        }
+          
+        return res.status(200).json({ UpdateCategory: updateCategory, message: 'update Category sucessfully'});
+            
+        } catch (error) {
+          const er = new Error('There is some error')
+          return res.status(400).json({ message: er.message });
+        }
+    })();
+
+}
+
+
+exports.deleteCategory = (req, res) => {
+    (async() => {
+        try {
+          /**
+            * joi validation
+            */
+           const schema = Joi.object({
+            categoryId: Joi.string().required(),
+        });
+
+        // schema options
+        const options = {
+            abortEarly: false, // include all errors
+            allowUnknown: true, // ignore unknown props
+        };
+
+        const { error, value } = schema.validate(req.params, options);
+
+        if (error) {
+            return res.status(400).json({ errorMessage: error.details });
+        }
+
+        if (req.role === 0) {
+            return res.status(400).json({ message: 'you can not delete Category. only admin can delete'})
+        }
+         
+        const categoryId = req.params.categoryId;
+
+        const deleteCategory = await Category.findById(categoryId);
+
+        if(isNull(deleteCategory)){
+            return res.status(422).json({ message: 'Category with given id is not exit.'});
+        }
+
+        await deleteCategory.deleteOne();
+        return res.status(200).json({ DeleteCategory: deleteCategory, message: 'Delete Category sucessfully'});
+            
+        } catch (error) {
+          const er = new Error('There is some error')
+          return res.status(400).json({ message: er.message });
+        }
+    })();   
+  }
--- routes/category.js
+++ routes/category.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const isAuth = require('../middleware/isAuth');
 
-const {createCategory} = require('../controllers/category');
+const {createCategory, getCategoryById, getCategory, updateCategory, deleteCategory }= require('../controllers/category');
 
 router.post('/create', isAuth, createCategory);
+router.get('/get/:categoryId', isAuth, getCategoryById);
+router.get('/get', isAuth, getCategory);
+router.put('/update/:categoryId', isAuth, updateCategory);
+router.delete('/delete/:categoryId', isAuth, deleteCategory);
 
 module.exports =  router;
